db91ffbff96b6415983583d8b3dbe835d92f2643
Qupci <qupcimeow@gmail.com>
2020-11-26 00:58:55 +0300

    Changed input Now meets requirements of the task

--- lab1v10.c
+++ lab1v10.c
@@ -17,27 +17,24 @@ void printVariant10(){
 int main(int argc, char **argv) {
   int cycleMethod = inputValue(argc,argv);
   if (cycleMethod >= 0){
-    printf("\n\n------------------------------------------------\nx: %lf\nn: %d\neps: %lf\ni: %d\n------------------------------------------------\n",x,n,eps,i);
+    printf("\n\n------------------------------------------------\nx: %lf",x);
+    if (cycleMethod == 1) {
+      printf("\nn: %d",n);
+    } else {
+      printf("\neps: %lf",eps);
+    }
+    printf("\ni: %d\n------------------------------------------------\n",x,n,eps,i);
     printf("\nsum:\t\tnew_x:\t\tstep:\t\t");
     n--; // last step == n
     answer = 1 / (pow((1-x),2)); // answer case for Variant 10
     switch (cycleMethod){
-      case 0: // use both n and eps as limiter
-        while (1) {
-          printVariant10();
-          if (new_x <= eps)
-            return end(0);
-          i++;
-          if (n < i)
-            return end(0);
-        }
-      case 1: // use eps as limiter
+      case 0: // use eps as limiter
         do {
           printVariant10();
           i++;
         } while (new_x >= eps);
         return end(0);
-      case 2: // use n as limiter
+      case 1: // use n as limiter
         for (i;i<=n;i++) {
           printVariant10();
         }
@@ -57,49 +54,47 @@ int main(int argc, char **argv) {
 }
 
 int inputValue(int argc, char **argv){
-  int fail = 0;
+  int method;
   if (argc != 1) {
     if (strncmp(argv[1],"--test",6) == 0 && argc > 2 ) {
       switch (atoi(argv[2])) {
         case 0:
           x = 0.5;
-          n = 100;
           eps = 0.001;
           return 0;
         case 1:
           x = 0.5;
           n = 20;
-          eps = 0.001;
-          return 0;
+          return 1;
       }
     } else if (argc == 4) {
       x = fabs(atof(argv[1]));
-      n = fabs(atoi(argv[2]));
-      eps = fabs(atof(argv[3]));
-      if (x > 1) {
-        printf("\nx (%lf) cannot be more than 1",x);
-        fail = 1;
-        }
-      if (eps > 1) {
-        printf("\neps (%lf) cannot be more than 1",eps);
-        fail = 1;
-        }
-      if (fail) {
-        return -2;
-      } else {
-        if (n == 0 && eps == 0) {
-          printf("\nAt least one limiter (n or eps) should be not zero.");
+      method = atoi(argv[2]);
+      if (method){
+        eps = fabs(atof(argv[3]));
+        if (eps > 1) {
+          printf("\neps (%lf) cannot be more than 1",eps);
           return -2;
-        } else if (n == 0) {
-          return 1;
-        } else if (eps == 0) {
-          return 2;
+        } else {
+          return 0; 
         }
-        return 0;
+      } else {
+        n = fabs(atoi(argv[3]));
+        if (x > 1) {
+          printf("\nx (%lf) cannot be more than 1",x);
+          return -2;
+        } else {
+          return 1;
+        }
       }
     } 
   } 
-  printf("ПИ 382007-в2 Ильюшенков Сергей Денисович\nЛабараторная 1, вариант 10\n\nusage: lab1v10 x n eps");
+  printf("ПИ 382007-в2 Ильюшенков Сергей Денисович\nЛабараторная 1, вариант 10");
+  printf("\n\nusage: lab1v10 x method [eps|n]");
+  printf("\n\nx\t\tfloat");
+  printf("\nmethod\t\tint\t0 exit by eps; 1 exit by n");
+  printf("\neps\t\tfloat");
+  printf("\nn\t\tint");
   printf("\n\n--test number\t\t0 exit by eps; 1 exit by n");
   return -1;
 } 
